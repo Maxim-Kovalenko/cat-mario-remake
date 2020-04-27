fc2a2d35ce90a9db87224391923f2c0ce89408f0
Max <mx.a.kovalenko@gmail.com>
2020-04-27 17:34:10 +0300

    Reforms
    My first program on C#

---
+++ hiworld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace EpicFPS
+{
+    class MyClass
+    {
+        public static void Main(string[] args)
+        {
+            var a = 2;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
